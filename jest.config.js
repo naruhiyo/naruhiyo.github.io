fce518751c4faa9f40d9e49c440500f2cf1b25fc
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-11 00:04:45 +0900

    Modified jest.

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  roots: ['./test'],
-  setupFilesAfterEnv: ['./test/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
   moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   transform: {
     '\\.(ts|tsx)$': 'ts-jest'
   },
   modulePaths: ['./test'],
-  testMatch: ['./test/**/*.(test|spec).+(ts|tsx)'],
+  testMatch: ['<rootDir>/test/spec/**/*.(test|spec).+(ts|tsx)'],
   collectCoverage: true,
   snapshotSerializers: ['enzyme-to-json/serializer']
 };
