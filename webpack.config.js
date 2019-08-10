2dc35bfa0585b7cb4bdc1465df2800a735fe5ffc
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-10 23:27:15 +0900

    Add lint and typescript and dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const environment = process.env.NODE_ENV || 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
-    entry: './src/main.ts',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015'],
-            },
-        }],
+  // If mode is "production", the app is optimized.
+  // If mode is "development", javascript files output with adding source map.
+  watch: !isProduction,
+  mode: environment,
+  devtool: isProduction ? false : 'source-map',
+  entry: {
+    main: [path.resolve(__dirname, 'src/main.tsx')]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      minify: {
+        html5: true,
+        minifyCSS: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        removeEmptyAttributes: true
+      },
+      inject: true,
+      chunksSortMode: 'dependency',
+      meta: {
+        title: 'NaruHiyo Pages',
+        keywords: 'NaruHiyo, Github Pages, Portfolio',
+        author: 'narugit, hiyoko3',
+        description:
+          'Naruhiyo is a project to develop some application such as mobile, desktop, webapp.',
+        'theme-color': '#a1d8e6',
+        'format-detection': 'telephone=no', //- Disabled phone number (iOS)
+        //- /* ==== Windows Theme ==== */
+        'msapplication-TileImage': '',
+        'msapplication-TileColor': '#a1d8e6'
+      }
+    })
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: isProduction ? '[name].[chunkhash].js' : '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial'
     }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader'
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 7777, // port number
+    contentBase: path.join(__dirname, 'dist/'), // Document root on server
+    progress: false, // Show progress on console.
+    inline: true, // The mode of inline.
+    hot: false, // use HMR
+    clientLogLevel: 'info', // The log level(none, error, warning, info)
+    historyApiFallback: true
+  }
+};
