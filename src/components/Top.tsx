824eeeac05368c6e96ccfe7a3526872ed79ac638
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-11 22:37:08 +0900

    Modified tcm script and add layouts.

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { useTranslation } from "react-i18next"
 import Footer from './layouts/Footer'
 import Header from './layouts/Header'
+import { Box, Grid } from 'grommet'
 
 export default () => {
 	const { t, i18n } = useTranslation()
@@ -9,7 +10,9 @@ export default () => {
 	return (
 		<div>
 			<Header />
-
+			<Box pad="small">
+				<p>Main contents</p>
+			</Box>
 			<Footer />
 		</div>
 	)
--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,7 +1,6 @@
 import { Text } from 'grommet'
 import * as React from "react"
 import { useTranslation } from "react-i18next"
-import * as style from '../../scss/layouts/Footer.scss'
 
 export default () => {
 	const { t, i18n } = useTranslation()
