619581a344e18acb69b139ed36259dac3cf3edbe
narugit <narusens@gmail.com>
2019-08-13 04:43:54 +0900

    Add test image

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,11 +1,14 @@
 import { Heading } from 'grommet'
 import * as React from 'react';
 import { useTranslation } from "react-i18next"
+import test from './test.png'
 
 export default () => {
 	const { t,i18n } = useTranslation()
 
-	return(
-		<Heading level="2" margin="small">{ t('header.title') }</Heading>
+	return (
+		<Heading level="2" margin="small">{t('header.title')}
+			<img src={test} alt="" />
+		</Heading>
 	);
 }
