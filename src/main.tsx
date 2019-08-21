5b81d1e8e75a942e3a1f449f683a90534180e7b8
hiyoko3 <sb.gm.0823@gmail.com>
2019-08-21 22:56:17 +0900

    Add webfonts.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom'
 import './plugins/i18n'
 
 import 'normalize.css'
+import './assets/scss/style.scss'
 import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
