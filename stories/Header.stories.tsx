5e29223eab9db61ec2c4a7d6f89949057906f308
hiyoko3 <sb.gm.0823@gmail.com>
2020-02-29 10:46:46 +0900

    rm build file and storybook

--- stories/Header.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import * as React from 'react'
-import Header from '../src/components/layouts/Header'
-import '../src/plugins/i18n'
-
-storiesOf('Header', module)
-.add('default Headder', () => <Header />)
