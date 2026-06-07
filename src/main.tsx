c80193a44e57ef3ce5cfda79190435941baf49f1
aecomet <16721102+aecomet@users.noreply.github.com>
2026-06-07 12:31:03 +0900

    fix: switch BrowserRouter to HashRouter to avoid 404 on reload

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from '@src/App';
 import '@src/assets/styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
